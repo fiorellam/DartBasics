51421e9d252f0d3dd222b4604a8a458b7cf2c22f
Fiorella <fiorellaroma98@gmail.com>
2024-12-05 21:40:14 -0800

    Funciones y clases

--- 05_functions.dart
+++ 05_functions.dart
@@ -1,11 +1,13 @@
-void main(){
- print(greetEveryone());
- print(greetEveryone2());
+void main() {
+  print(greetEveryone());
+  print(greetEveryone2());
   print('Suma: ${addTwoNumbers(7, 5)}');
   print('Suma flecha: ${addTwoNumbers2(4, 3)}');
+
+  print(greetPerson(name: 'Fiore', message: 'Holi'));
 }
 
-String greetEveryone(){
+String greetEveryone() {
   return 'Hello Everyone!';
 }
 
@@ -17,8 +19,11 @@ int addTwoNumbers(int a, int b) {
 
 int addTwoNumbers2(int a, int b) => a + b;
 
-int addTwoNumbersOptional(int a, [int b = 0]){
+int addTwoNumbersOptional(int a, [int b = 0]) {
   //b ??= 0;
   return a + b;
 }
 
+String greetPerson({required String name, String message = 'Hola'}) {
+  return '$message, Fiore';
+}
